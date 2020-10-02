c9b4c41c45d284fa91d8ddc8484c319086beca80
ZeProf2C <mail@normanalie.fr>
2020-10-02 10:46:43 +0400

    func strToPostfix, in work

--- postfix.py
+++ postfix.py
@@ -1,29 +1,50 @@
-#Calcul d'expressions postfixées simples en python pour le programme de Terminale NSI
+#title          :postfix.py
+#description    :Calcul d'expressions postfixées simples en python pour le programme de Terminale NSI
+#author         :NALIE
+#version        :1.0 
+#==============================================================================
+
+# -*- coding: utf-8 -*-
 #[3, 4, 5, 6, "-", "*", "+"] = 7   [3, 4, "+", 5, "*", 6, "-"] = -29
+
+def strToPostfix(string):
+    string = list(string)
+    postfixExpression = list()
+
+    for i in string:
+        if int(i):
+            postfixExpression.append(int(i))
+        else:
+            postfixExpression.append(str(i))
+
+    return postfixExpression
 
 
 def postfixe_eval(expression):
-    postfixe = list(expression) #Expression à calculer, utiliser comme une pile
-    postfixeCache = [] #Cache des nombres à calculer
+    if len(expression) == 0: return None #Si l'expression est vide, on ne va pas plus loin
+
+    postfixe = list(expression) #Expression à calculer, utiliser comme une pile. On copie le tableau.
+    postfixeCache = list() #Cache des nombres à calculer
 
     while len(postfixe) > 1:
         while type(postfixe[0]) != str: #On dépile postfixe et on empile de postfixeCache 
             postfixeCache.insert(0, postfixe[0])
             postfixe.pop(0)
 
-        calc = postfixe[0] #On dépile le calcul à réaliser dans une variable
+        operand = postfixe[0] #On dépile l'opérateur à réaliser dans une variable
         postfixe.pop(0)
 
-
         #On calcul et on empile dans postfixe
-        if calc == "+":
+        if operand == "+":
             postfixe.insert(0, postfixeCache[0] + postfixeCache[1])
-        elif calc == "-":
+        elif operand == "-":
             postfixe.insert(0, postfixeCache[0] - postfixeCache[1])
-        elif calc == "*":
+        elif operand == "*":
             postfixe.insert(0, postfixeCache[0] * postfixeCache[1])
-        elif calc == "/":
+        elif operand == "/":
             postfixe.insert(0, postfixeCache[0] / postfixeCache[1])
+        else:
+            assert False, "Opérateur " + str(operand) +  " inconnu"
 
         postfixeCache.pop(0) #On enlève les deux derniers éléments de la pile de cache. On vient de les calculer
         postfixeCache.pop(0)
@@ -32,3 +53,5 @@ def postfixe_eval(expression):
     return postfixe
 
 print(postfixe_eval([3, 4, "+", 5, "*", 6, "-"]))
+print(postfixe_eval([3, 4, 5, 6, "-", "*", "+"]))
+print(strToPostfix("3456-*+"))
