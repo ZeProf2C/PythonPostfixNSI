cf98fe7dd00adf016f26126282128a8798cd08cb
ZeProf2C <mail@normanalie.fr>
2020-10-02 08:45:20 +0400

    add new expression

--- postfix.py
+++ postfix.py
@@ -1,4 +1,6 @@
 #Calcul d'expressions postfixées simples en python pour le programme de Terminale NSI
+#[3, 4, 5, 6, "-", "*", "+"] = 7   [3, 4, "+", 5, "*", 6, "-"] = -29
+
 
 postfixe = [3, 4, "+", 5, "*", 6, "-"] #Expression à calculer, utiliser comme une pile
 postfixeCache = [] #Cache des nombres à calculer
@@ -12,6 +14,7 @@ while len(postfixe) > 1:
     expression = postfixe[i] #On dépile le calcul à réaliser dans une variable
     postfixe.pop(i)
 
+
     #On calcul et on empile dans postfixe
     if expression == "+":
         postfixe.insert(0, postfixeCache[0] + postfixeCache[1])
@@ -21,5 +24,6 @@ while len(postfixe) > 1:
         postfixe.insert(0, postfixeCache[0] * postfixeCache[1])
     elif expression == "/":
         postfixe.insert(0, postfixeCache[0] / postfixeCache[1])
+
 
 print(postfixe)
