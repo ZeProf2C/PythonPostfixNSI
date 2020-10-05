c4d2c15db053b20821041ee33c2f0fcd344de00e
ZeProf2C <mail@normanalie.fr>
2020-10-05 16:20:40 +0400

    naming

--- postfix.py
+++ postfix.py
@@ -7,53 +7,53 @@
 # -*- coding: utf-8 -*-
 #[3, 4, 5, 6, "-", "*", "+"] = 7   [3, 4, "+", 5, "*", 6, "-"] = -29
 
-def strToPostfix(string):
+def str_to_postfix(string):
     string = list(string)
-    postfixExpression = []
+    postfixxpression = []
     for i in string:
         try:
             int(i)
-            postfixExpression.append(int(i))
+            postfixxpression.append(int(i))
         except ValueError:
-            postfixExpression.append(i)
+            postfixxpression.append(i)
 
-    return postfixExpression
+    return postfixxpression
 
 
-def postfixe_eval(expression):
+def postfix_eval(expression):
     if len(expression) == 0: return None #Si l'expression est vide, on ne va pas plus loin
 
-    if(type(expression) == str): expression = strToPostfix(expression) #Si c'est un str on le convertit en array
+    if(type(expression) == str): expression = str_to_postfix(expression) #Si c'est un str on le convertit en array
 
-    postfixe = list(expression) #Expression à calculer, utiliser comme une pile. On copie le tableau.
-    postfixeCache = list() #Cache des nombres à calculer
+    postfix = list(expression) #Expression à calculer, utiliser comme une pile. On copie le tableau.
+    postfixCache = list() #Cache des nombres à calculer
 
-    while len(postfixe) > 1:
-        while type(postfixe[0]) != str: #On dépile postfixe et on empile de postfixeCache 
-            postfixeCache.insert(0, postfixe[0])
-            postfixe.pop(0)
+    while len(postfix) > 1:
+        while type(postfix[0]) != str: #On dépile postfix et on empile de postfixCache 
+            postfixCache.insert(0, postfix[0])
+            postfix.pop(0)
 
-        operand = postfixe[0] #On dépile l'opérateur à réaliser dans une variable
-        postfixe.pop(0)
+        operand = postfix[0] #On dépile l'opérateur à réaliser dans une variable
+        postfix.pop(0)
 
-        #On calcul et on empile dans postfixe
+        #On calcul et on empile dans postfix
         if operand == "+":
-            postfixe.insert(0, postfixeCache[0] + postfixeCache[1])
+            postfix.insert(0, postfixCache[0] + postfixCache[1])
         elif operand == "-":
-            postfixe.insert(0, postfixeCache[0] - postfixeCache[1])
+            postfix.insert(0, postfixCache[0] - postfixCache[1])
         elif operand == "*":
-            postfixe.insert(0, postfixeCache[0] * postfixeCache[1])
+            postfix.insert(0, postfixCache[0] * postfixCache[1])
         elif operand == "/":
-            postfixe.insert(0, postfixeCache[0] / postfixeCache[1])
+            postfix.insert(0, postfixCache[0] / postfixCache[1])
         else:
             assert False, "Opérateur " + str(operand) +  " inconnu"
 
-        postfixeCache.pop(0) #On enlève les deux derniers éléments de la pile de cache. On vient de les calculer
-        postfixeCache.pop(0)
+        postfixCache.pop(0) #On enlève les deux derniers éléments de la pile de cache. On vient de les calculer
+        postfixCache.pop(0)
 
-    postfixe = int(postfixe[0])
-    return postfixe
+    postfix = int(postfix[0])
+    return postfix
 
-print(postfixe_eval([3, 4, "+", 5, "*", 6, "-"]))
-print(postfixe_eval([3, 4, 5, 6, "-", "*", "+"]))
-print(postfixe_eval("3456-*+"))
+print(postfix_eval([3, 4, "+", 5, "*", 6, "-"]))
+print(postfix_eval([3, 4, 5, 6, "-", "*", "+"]))
+print(postfix_eval("3456-*+"))
